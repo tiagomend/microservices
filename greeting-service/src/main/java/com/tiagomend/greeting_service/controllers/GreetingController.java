fb16d6bc9dd72d98806f4f57fafdba7cc965e4a3
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2024-10-22 13:23:14 -0300

    feat: creation of the greeting-service microservice

---
+++ greeting-service/src/main/java/com/tiagomend/greeting_service/controllers/GreetingController.java
@@ -0,0 +1,31 @@
+package com.tiagomend.greeting_service.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tiagomend.greeting_service.configuration.GreetingConfiguration;
+import com.tiagomend.greeting_service.model.Greeting;
+
+@RestController
+public class GreetingController {
+    private static final String template = "%s, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    GreetingConfiguration config;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "") String name) {
+        if (name.isEmpty()) {
+            name = config.getDefaultValue();
+        }
+        return new Greeting(
+                counter.incrementAndGet(),
+                String.format(template, config.getGreeting(), name));
+    }
+}
---
+++ greeting-service/src/test/java/com/tiagomend/greeting_service/GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagomend.greeting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
